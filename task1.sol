956bebad8c854657fc64ce42cb7007951fa3bba0
n3bl <143548334+n3bl@users.noreply.github.com>
2023-09-01 08:47:50 +0300

    Create task1.sol

---
+++ task1.sol
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/train/solidity
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function expressionMatter(int a, int b, int c) public pure returns (int) {
+    // your code here
+  }
+}
