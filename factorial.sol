038e34c5fa6a5a45507267501f4361ba2f42b725
n3bl <143548334+n3bl@users.noreply.github.com>
2023-09-02 22:44:31 +0300

    Create factorial.sol

---
+++ factorial.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+// Return true if it is a factor or false if it is not.
+contract Kata {
+  function checkForFactor(int base, int factor) public pure returns (bool) {
+    return true;
+  }
+}
